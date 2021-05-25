d0449bd79abb1d29b4704afebf4c9167a634fa84
ShenXiaowei <shenxiaowei@growingio.com>
2021-05-25 14:11:59 +0800

    feat：添加详情页

---
+++ pages/detail/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="u-margin-top-60">
+    这是详情页
+    <u-button @click="handleBack">返回主页</u-button>
+  </view>
+</template>
+
+<script>
+export default {
+  onShow() {
+    console.log("详情页 onShow");
+  },
+	onShareAppMessage() {
+		console.log('发起了分享');
+		return {
+			title: '分享标题',
+			path: '/pages/detail/index'
+		};
+	},
+  methods: {
+    handleBack() {
+      uni.navigateBack({ url: "../home/index" });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
